70bc6974fca48b9b47884628d462ca0f0dae0ab0
Luna <lunacodes@gmail.com>
2019-10-11 15:57:22 -0400

    Move 'Assume CNAME Unchaged' to later in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # Don't mess with the CNAME File!!!!
-git update-index --skip-worktree CNAME
 
 # Add Staging remote & Pull all of the branches
 git remote add staging https://github.com/pgpconference/staging.git
 git checkout -b staging
 git fetch --all
 gb -u staging/gh-pages
-git pull staging
+git pull
 
+git update-index --skip-worktree CNAME
 # Create and checkout local staging branch
 # Tell staging to track Remote staging:gh-pages
 # set `push.default` to always have staging push to staging:gh-pages.
