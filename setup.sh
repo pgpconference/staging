f9a796d953d4b8648d1423dc59069f83729aa35c
Luna <lunacodes@gmail.com>
2019-10-11 15:38:59 -0400

    Fix staging setup issues

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 git update-index --skip-worktree CNAME
 
 # Add Staging remote & Pull all of the branches
-git remote add staging https://github.com/pgpglobal/staging.git
+git remote add staging https://github.com/pgpconference/staging.git
+git checkout -b staging
+gb -u staging/gh-pages
 git pull staging
 
 # Create and checkout local staging branch
-  `git checkout -b staging`
 # Tell staging to track Remote staging:gh-pages
-  `gb -u staging/gh-pages`
 # set `push.default` to always have staging push to staging:gh-pages.
 git config push.default upstream
