5ff791028ba19251620a240845d74b88b77a0c7c
Luna <lunacodes@gmail.com>
2019-10-11 17:02:59 -0400

    Improve comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
-# Add Staging remote & Pull all of the branches
+# Add Staging remote & fetch all changes from both remotes
 git remote add staging https://github.com/pgpconference/staging.git
-git checkout -b staging
 git fetch --all
+
+# Create and checkout local staging branch
+git checkout -b staging
+# Tell staging to track Remote staging:gh-pages
 gb -u staging/gh-pages
+
+# Pull any changes for the new branch
 git pull
 
 # Don't mess with the CNAME File!!!!
 git update-index --skip-worktree CNAME
-# Create and checkout local staging branch
-# Tell staging to track Remote staging:gh-pages
+
 # set `push.default` to always have staging push to staging:gh-pages.
 git config push.default upstream
