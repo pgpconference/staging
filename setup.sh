06d650b3db938ea00703ed6e61d93f503d31c719
Luna <lunacodes@gmail.com>
2019-11-28 05:55:42 -0500

    Really ensure users only have staging branch

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ git update-index --skip-worktree CNAME
 
 # Add Staging remote & fetch all changes from both remotes
 git remote add staging https://github.com/pgpconference/staging.git
+# git remote remove origin
 git fetch --all
-git branch -d gh-pages master 
+git checkout -b staging
+git branch -d gh-pages master
 
 # Create and checkout local staging branch
 git checkout -b staging
@@ -13,6 +15,9 @@ gb -u staging/staging
 
 # Pull any changes for the new branch
 git pull
+
+# Remove any lingering traces of master and gh-pages branches
+git fetch --prune
 
 # set `push.default` to always have staging push to staging:gh-pages.
 git config push.default upstream
