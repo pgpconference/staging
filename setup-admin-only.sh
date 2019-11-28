3af027d2297264fb73ec6f1ded4366524314602d
Luna <lunacodes@gmail.com>
2019-11-28 05:58:47 -0500

    Add seperate setup file for repo admins/team members

---
+++ setup-admin-only.sh
@@ -0,0 +1,30 @@
+# Don't mess with the CNAME File!!!!
+git update-index --skip-worktree CNAME
+
+# Add Staging remote & fetch all changes from both remotes
+git remote add staging https://github.com/pgpconference/staging.git
+git remote add admin https://github.com/pgpconference/staging.git
+
+git fetch --all
+git checkout -b staging
+
+# Remove master branch b/c there's no point in having it
+git branch -d master
+
+# Create and checkout local staging branch
+git checkout -b staging
+# Tell staging to track Remote staging:gh-pages
+gb -u staging/staging
+
+git checkout -b gh-pages
+# Tell gh-pages to track Remote staging:gh-pages
+gb -u staging/gh-pages
+
+# Pull any changes for the new branch
+git pull
+
+# Remove any lingering traces of master and gh-pages branches
+git fetch --prune
+
+# set `push.default` to always have staging push to staging:gh-pages.
+git config push.default upstream
