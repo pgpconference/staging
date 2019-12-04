2f231f97d6e74aabb5d5abe292fde60aaf4f8c78
Luna <lunacodes@gmail.com>
2019-12-04 04:09:34 -0500

    Add Docs fetching to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 # Don't mess with the CNAME File!!!!
 git update-index --skip-worktree CNAME
 
+# Tell Git to ignore _config_local.yml - so we can change whatever we want.
+git update-index --skip-worktree _config_local.yml
+
+# Get the Docs
+git submodule update --init
+
 # Add Staging remote & fetch all changes from both remotes
 git remote add staging https://github.com/pgpconference/staging.git
 git fetch --all
@@ -8,10 +14,10 @@ git fetch --all
 # Create and checkout local staging branch
 git checkout -b staging
 # Tell staging to track Remote staging:gh-pages
-gb -u staging/gh-pages
+git branch -u staging/gh-pages
 
 # Pull any changes for the new branch
-git pull
+git pull --all
 
 # set `push.default` to always have staging push to staging:gh-pages.
 git config push.default upstream
